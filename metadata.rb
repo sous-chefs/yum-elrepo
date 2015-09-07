3cfd77068bc012636f62c27131cbfada7af1e8d3
Tim Smith <tsmith@chef.io>
2015-09-07 13:24:48 -0700

    Update maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'yum-elrepo'
-maintainer       'Chef'
-maintainer_email 'Sean OMeara <someara@getchef.com>'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/Configures yum-elrepo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
