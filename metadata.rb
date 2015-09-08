2d11ebf243aecd8eb6c7c6bb5cf15bd9e12f06c1
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:08:19 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.2'
 
 depends 'yum', '~> 3.0'
+
+source_url 'https://github.com/chef-cookbooks/yum-elrepo' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-elrepo/issues' if respond_to?(:issues_url)
