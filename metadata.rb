01e992f0142e7274a27e2018a5eac8acf988f5a9
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:12:40 -0700

    Rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'yum-elrepo'
-maintainer       'Chef Software, Inc.'
+name 'yum-elrepo'
+maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
-description      'Installs/Configures yum-elrepo'
+license 'Apache 2.0'
+description 'Installs/Configures yum-elrepo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version '0.2.2'
 
 depends 'yum', '~> 3.0'
 
