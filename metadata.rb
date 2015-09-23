99f3ea770d1bbaf3d955bed7d72b60fa7d3c2d43
Tim Smith <tsmith84@gmail.com>
2015-09-22 23:41:47 -0700

    Improve description, bump requirement to yum 3.2

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,15 @@ name 'yum-elrepo'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Installs/Configures yum-elrepo'
+description 'Installs and configures the elrepo yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.2'
 
-depends 'yum', '~> 3.0'
+depends 'yum', '~> 3.2'
 
 source_url 'https://github.com/chef-cookbooks/yum-elrepo' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/yum-elrepo/issues' if respond_to?(:issues_url)
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
