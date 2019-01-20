7449d342c9ce447cb275e32a8156517b49394613
Tim Smith <tsmith@chef.io>
2019-01-20 12:09:28 -0800

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/testing.rb
+++ attributes/testing.rb
@@ -1,4 +1,3 @@
-
 default['yum']['elrepo-testing']['mirrorlist'] = "http://elrepo.org/mirrors-elrepo-testing.el#{node['platform_version'].to_i}"
 default['yum']['elrepo-testing']['description'] = 'ELRepo.org Testing Yum Repository'
 default['yum']['elrepo-testing']['gpgkey'] = 'http://elrepo.org/RPM-GPG-KEY-elrepo.org'
