08210714355ba0ae2fe473493cd90cea180eac58
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:57:31 -0700

    Resolve Cookstyle 5.8 warnings
    Resolve the latest linting warnings
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures the elrepo yum repository'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
@@ -12,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-elrepo'
 issues_url 'https://github.com/chef-cookbooks/yum-elrepo/issues'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.14'
