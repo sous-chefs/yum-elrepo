1656abc35f1987cef3b56dfdf41ef98e0c48de14
Ryan <kennedy@osuosl.org>
2021-03-09 16:00:39 -0800

    update inspec

--- attributes/extras.rb
+++ attributes/extras.rb
@@ -1,4 +1,4 @@
-default['yum']['elrepo-extras']['mirrorlist'] = "http://elrepo.org/mirrors-elrepo-extras.el#{node['platform_version'].to_i}"
+default['yum']['elrepo-extras']['mirrorlist'] = "http://elrepo.org/mirrors-elrepo-extras.el#{platform_family?('amazon') ? '7' : node['platform_version'].to_i}"
 default['yum']['elrepo-extras']['description'] = 'ELRepo.org Extras Yum Repository'
 default['yum']['elrepo-extras']['gpgkey'] = 'http://elrepo.org/RPM-GPG-KEY-elrepo.org'
 default['yum']['elrepo-extras']['enabled'] = true
--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,5 +1,37 @@
-describe file('/etc/yum.repos.d/elrepo.repo') do
+e_rel = os.name == 'amazon' ? '7' : os.release.to_i
+
+describe yum.repo('elrepo') do
   it { should exist }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
+  it { should be_enabled }
+  its('mirrors') { should cmp "http://elrepo.org/mirrors-elrepo.el#{e_rel}" }
+end
+
+describe yum.repo('elrepo-extras') do
+  it { should exist }
+  it { should be_enabled }
+  its('mirrors') { should cmp "http://elrepo.org/mirrors-elrepo-extras.el#{e_rel}" }
+end
+
+describe yum.repo('elrepo-kernel') do
+  it { should exist }
+  it { should be_enabled }
+  its('mirrors') { should cmp "http://elrepo.org/mirrors-elrepo-kernel.el#{e_rel}" }
+end
+
+describe yum.repo('elrepo-testing') do
+  it { should exist }
+  it { should be_enabled }
+  its('mirrors') { should cmp "http://elrepo.org/mirrors-elrepo-testing.el#{e_rel}" }
+end
+
+%w(
+  elrepo
+  elrepo-extras
+  elrepo-kernel
+  elrepo-testing
+).each do |repo|
+  describe ini("/etc/yum.repos.d/#{repo}.repo") do
+    its("#{repo}.gpgcheck") { should cmp 1 }
+    its("#{repo}.gpgkey") { should cmp 'http://elrepo.org/RPM-GPG-KEY-elrepo.org' }
+  end
 end
